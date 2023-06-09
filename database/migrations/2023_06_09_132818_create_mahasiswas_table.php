f6df60510fca3963c4399108cd351f574294cceb
Varogell <alvaroivanka@gmail.com>
2023-06-09 21:28:22 +0700

    first commit

---
+++ database/migrations/2023_06_09_132818_create_mahasiswas_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('mahasiswas', function (Blueprint $table) {
+            $table->bigIncrements('Nim');
+            $table->string('Nama');
+            $table->string('Kelas');
+            $table->string('Jurusan');
+            $table->string('No_Handphone');
+            $table->string('email');
+            $table->string('tanggal_lahir');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('mahasiswas');
+    }
+};
